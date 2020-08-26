3b68a0217c34f541462aa4f97de5e43baa714ab5
Erika Suárez Valencia <esuarez@humboldt.org.co>
2020-08-26 15:30:26 -0500

    Add entrypoint to execute app with R or Rscript command

--- server.r
+++ server.r
@@ -7,12 +7,11 @@ library(sf)
 library(dplyr)
 library(stringi)
 
-
+source("db_to_dwc_simp.r")
 shinyServer(function(input, output, session) {
   volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
   shinyDirChoose(input, "directory", roots = volumes, session = session, restrictions = system.file(package = "base"), filetypes=c('', 'gdb'))
 
- 
   dsimput<- reactive({
     req(input$directory)
     if (is.integer(input$directory)) {
@@ -20,18 +19,18 @@ shinyServer(function(input, output, session) {
     } else {
       df<-parseDirPath(volumes, input$directory)
       suppressWarnings(db_to_dwc(df))
-    } 
+    }
  })
   fileext<- reactive({
     switch (input$type,
             "Excel (CSV)" = "csv", "Texto (TSV)" = "txt", "Text (Separado por espacios)" = "txt")
-    
+
   })
   ## Tabla en pantalla
   output$outTable <- renderTable({
     dsimput()
   })
- 
+
   # descargar los datos en el formato seleccionado ----
   output$downloadData <- downloadHandler(
     filename = function() {
@@ -43,5 +42,5 @@ shinyServer(function(input, output, session) {
       write.csv(dsimput(), file, row.names = FALSE)
     }
   )
-  
+
 })
