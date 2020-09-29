1107fd68edce6a9eaf08728f23ac1925ab229604
Cristian Alexander Cruz Rodriguez <ccruz@humboldt.org.co>
2020-09-29 13:02:13 -0500

    Se ajustaron los textos de la herramienta

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(shinyFiles)
 library(leaflet)
 
 shinyUI(pageWithSidebar(
-  headerPanel("Herramienta para verificar calidad de datos"),
+  headerPanel("Herramienta para verificar la calidad de datos"),
   sidebarPanel(img(src = "logo.png",  style = "width:25%;"),
                p(),
                hr(),
@@ -18,15 +18,14 @@ shinyUI(pageWithSidebar(
                helpText("Descargue los datos unificados"),
                radioButtons("type", "",
                             choices = c("Excel (CSV)", "Texto (TSV)", "Texto (Separado por espacios)")),
-               downloadButton("downloadData", "Guardar como")
+               downloadButton("downloadData", "Descargar")
                ),
 
                conditionalPanel(condition="input.tabselected==2",
                                 #definicion columnas coordenadas
 
                                 #Datos divipola
-                                tags$hr(),
-                                tags$strong("trasformacion datos Divipola"),
+                                tags$strong("Trasformacion datos Divipola"),
                                 radioButtons(inputId = "dividata",
                                              label = "",
                                              choices = c("Si" = 1, "No" = 2),
@@ -61,17 +60,18 @@ shinyUI(pageWithSidebar(
                                 helpText("Descargue los datos transformados"),
                                 radioButtons("type2", "",
                                              choices = c("Excel (CSV)", "Texto (TSV)", "Texto (Separado por espacios)")),
-                                downloadButton("downloadData2", "Guardar como")
+                                downloadButton("downloadData2", "Descargar")
                                  ),
 
                 conditionalPanel(condition="input.tabselected==3",
-                                fileInput('verCsv', 'Choose CSV File',
+                                 helpText("Seleccione el archivo obtenido en el paso anterior, o uno nuevo que cuente con la columna 'scientificName'"),
+                                 fileInput('verCsv', "Seleccione el archivo 'csv'",
                                   accept=c('text/csv',
                                   'text/comma-separated-values,text/plain',
                                   '.csv')
                                 ),
                                 tags$hr(),
-                                checkboxInput('header', 'Header', TRUE),
+                                checkboxInput('header', 'Encabezado', TRUE),
                                 radioButtons(
                                   'sep',
                                   'Separator',
@@ -84,19 +84,14 @@ shinyUI(pageWithSidebar(
                                 actionButton("mergeBtn", "Mezclar resultados"),
                                 tags$hr(),
                                 helpText("Descargue los datos validados"),
-                                downloadButton("downloadData3", "Guardar como")
+                                downloadButton("downloadData3", "Descargar")
                 ),
 
                 conditionalPanel(condition="input.tabselected==4",
-                                p("Este modulo permite verificar que el municipio
-                                  y departamento asociado a cada registro corresponda
-                                  con sus coordenadas. El proceso agrega unas columnas
-                                  con municipios y departamentos recomendados para
-                                  aquellos registros donde no hay una congruencia. Para
-                                  ejecutar la verificacion asegurese de subir una tabla
-                                  con los datos estructurados en formato csv y haga click
-                                  en el boton de Verificar datos."),
-                                fileInput("gvInput", "Seleccione el archivo CSV",
+                                 helpText("Seleccione el archivo obtenido en cualquier de los pasos anteriores, o
+                                 uno que cuente con los campos geograficos DwC ('country', 'stateProvince,'
+                                          'country', 'decimalLongitude', 'decimalLatitude')."),
+                                 fileInput("gvInput", "Seleccione el archivo CSV",
                                           multiple = FALSE,
                                           accept = c("text/csv",
                                                      "text/comma-separated-values,text/plain",
@@ -104,31 +99,30 @@ shinyUI(pageWithSidebar(
                                           ),
                                 actionButton("runGV", "Verificar datos"),
                                 p(),
+                                tags$hr(),
+                                helpText("Descargue los datos verificados"),
                                 downloadButton("downloadGVOutput", "Descargar"),
-                                hr(),
-                                p("Modulo creado por Ivan Gonzalez, Laura Carolina Bello,
-                                   Maria Cecilia Londono-Murcia y Jorge Velasquez-Tibata")
-                  ),
+                                ),
 
                 conditionalPanel(condition="input.tabselected==5",
-                                h1("Ingrese puntos:"),
-                                numericInput("long", label = h3("Longitud:"), value = -73.839222),
-                                numericInput("lat", label = h3("Latitud:"), value = 4.721806),
+                                 helpText("Este modulo es una herramienta desarrollada
+                                  para generar listados preliminares de especies
+                                  a partir de la consulta de los mapas validados
+                                  disponibles en BioModelos."),
+                                 hr(),
+                                 p("Seleccione las coordenadas del punto del cual desea conocer las especies potenciales"),
+                                numericInput("long", label = ("Longitud:"), value = -73.839222),
+                                numericInput("lat", label = ("Latitud:"), value = 4.721806),
                                 actionButton("locality", "Mapear"),
                                 p(),
                                 downloadButton("downloadDataBioMAD", "Descargar"),
                                 p(),
-                                p("Este modulo es una herramienta desarrollada
-                                  para generar listados preliminares de especies
-                                  a partir de la consulta de los mapas validados
-                                  disponibles en BioModelos."),
+                                p(),
                                 p("Se sugiere usar con prudencia los listados generados.
                                   Si desea conocer las distribuciones de las especies listadas,
                                   por favor consulte",
                                   a("BioModelos.", href = "http://biomodelos.humboldt.org.co/")),
-                                hr(),
-                                "Modulo creado por Elkin A. Noguera-Urbano"
-               )
+                               )
 
   ),# sidebarPanel bracket
 
@@ -145,18 +139,35 @@ shinyUI(pageWithSidebar(
            y los archivos geográficos deben mantener los nombres definidos por la Agencia \n
            Nacional de Licencias Ambientales (ANLA)"),
                tags$p(HTML("Datos identificados y migrados a la plantilla DwC, recuerde que dependiendo el volumen de los datos, este proceso puede tardar varios minutos\n")),
+               tags$br(),
+               tags$br(),
+               tags$p(HTML("Cite este recurso de la siguiente forma:")),
+               tags$blockquote("Bello C., Castro C., Cruz-Rodriguez C., Gonzalez I, Londono M. C., Lopez D., Noguera-Urbano E. A., Olaya-Rodriguez M. H., Rey J. C., Suarez E., Velasquez-Tibata J., Villa M. 2020. Herramienta para verificar la calidad de datos sobre biodiversidad. Instituto de Investigacion de Recussos Biologicos, Alexander von Humboldt. Esta aplicacion fue creada en el marco de la suborden de tarea 4 en el marco del Proyecto Riqueza Natural de USAID.", style = "font-size:90%"),
+               
                div(dataTableOutput("initTable"), style = "font-size:80%")),
 
       tabPanel("Estructuracion de los datos", value=2,
-               tags$p(HTML("Datos\n")),
+               tags$br(),
+               tags$em(HTML("Para continuar con el proceso de verificacion usando la informacion extraida de las GDB, seleccione 'si' y 'agregar' en el panel de opciones")),
                div(dataTableOutput("tbTranform"), style = "font-size:80%")
       ),
 
       tabPanel("Verificacion Taxonomica", value = 3,
+               tags$br(),
+                tags$em(HTML("En esta seccion podra verificar la coincidencia entre los nombres cientificos que provienen de la gdb u otra fuente, con validadores taxonomicos disponibles en la web, y los mezcla con la tabla de entrada")),
                div(tableOutput("estrTable"), style = "font-size:80%")
       ),
 
       tabPanel('Verificacion Geografica', value = 4,
+               tags$br(),
+               tags$em(HTML("Este modulo permite verificar que el municipio
+                            y departamento asociado a cada registro corresponda
+                            con sus coordenadas. El proceso agrega unas columnas
+                            con municipios y departamentos recomendados para
+                            aquellos registros donde no hay una congruencia. Para
+                            ejecutar la verificacion asegurese de subir una tabla
+                            con los datos estructurados en formato csv y haga click
+                            en el boton de Verificar datos.")),
                div(dataTableOutput("gvOutput"), style = "font-size:80%")
       ),
 
